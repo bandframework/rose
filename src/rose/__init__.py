f5824a178472dba17e5707324313d055343e7d8a
Daniel Odell <dmcodelljr@gmail.com>
2022-10-06 11:17:28 -0400

    Expose phase_shift function in free_solutions.

--- src/rose/__init__.py
+++ src/rose/__init__.py
@@ -2,3 +2,4 @@ from .interaction import Interaction, MN_Potential
 from .schroedinger import SchroedingerEquation
 from . import constants
 from .reduced_basis_emulator import ReducedBasisEmulator
+from .free_solutions import phase_shift

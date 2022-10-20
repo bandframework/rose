dc28348379878ac6d38bfef65a055bb210d7f9de
Daniel Odell <dmcodelljr@gmail.com>
2022-10-20 11:24:58 -0400

    Update phase shift method to handle interpolation internally.

--- src/rose/free_solutions.py
+++ src/rose/free_solutions.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.special import spherical_jn, spherical_yn
+from scipy.interpolate import interp1d
 from scipy.misc import derivative
 
 def F(rho, ell):
@@ -26,8 +27,17 @@ def H_minus_prime(rho, ell, dx=1e-6):
     return derivative(lambda z: H_minus(z, ell), rho, dx=dx)
 
 
-def phase_shift(u, up, ell, x0):
-    rl = 1/x0 * (u/up)
+# def phase_shift(u, up, ell, x0):
+#     rl = 1/x0 * (u/up)
+#     return np.log(
+#         (H_minus(x0, ell) - x0*rl*H_minus_prime(x0, ell)) / 
+#         (H_plus(x0, ell) - x0*rl*H_plus_prime(x0, ell))
+#     ) / 2j
+
+
+def phase_shift(u, s, ell, x0, dx=1e-6):
+    u_func = interp1d(s, u, kind='cubic')
+    rl = 1/x0 * (u_func(x0)/derivative(u_func, x0, dx=dx))
     return np.log(
         (H_minus(x0, ell) - x0*rl*H_minus_prime(x0, ell)) / 
         (H_plus(x0, ell) - x0*rl*H_plus_prime(x0, ell))

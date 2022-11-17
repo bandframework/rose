1eb1ad4f99480c4c9189bdd240021868c3fb28f5
Daniel Odell <dmcodelljr@gmail.com>
2022-11-17 11:43:25 -0500

    Expose metrics module.

--- src/rose/__init__.py
+++ src/rose/__init__.py
@@ -1,5 +1,5 @@
 from .interaction import Interaction, MN_Potential
 from .schroedinger import SchroedingerEquation
-from . import constants
+from . import constants, metrics
 from .reduced_basis_emulator import ReducedBasisEmulator
 from .free_solutions import phase_shift

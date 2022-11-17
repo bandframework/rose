b8f6556122f66b75419422cee465872ffc9d8405
Daniel Odell <dmcodelljr@gmail.com>
2022-11-17 11:47:02 -0500

    Clean up.

--- src/rose/reduced_basis_emulator.py
+++ src/rose/reduced_basis_emulator.py
@@ -7,10 +7,10 @@ import numpy as np
 import numpy.typing as npt
 
 from .interaction import Interaction
-from .schroedinger import *
+from .schroedinger import SchroedingerEquation, DEFAULT_R_MIN, DEFAULT_R_MAX, DEFAULT_R_0, DEFAULT_NUM_PTS
 from .basis import RelativeBasis
 from .constants import HBARC
-from .free_solutions import phase_shift, phase_shift_interp
+from .free_solutions import phase_shift
 from .utility import finite_difference_first_derivative, finite_difference_second_derivative
 
 # How many points should be ignored at the beginning
@@ -132,53 +132,3 @@ class ReducedBasisEmulator:
         phi = np.sum(np.hstack((1, x)) * self.phi_components[self.i_0, :])
         phi_prime = np.sum(np.hstack((1, x)) * self.phi_prime_components[self.i_0, :])
         return phase_shift(phi, phi_prime, self.l, self.s_mesh[self.i_0])
-
-
-    # def wave_function_metric(self,
-    #     filename: str
-    # ):
-    #     with open(filename, 'rb') as f:
-    #         benchmark_data = pickle.load(f)
-        
-    #     thetas = np.array([bd.theta for bd in benchmark_data])
-    #     wave_functions = np.array([bd.phi for bd in benchmark_data])
-    #     emulated_wave_functions = np.array([self.emulate_wave_function(theta) for theta in thetas])
-    #     abs_residuals = np.abs(emulated_wave_functions - wave_functions)
-    #     norm = np.sqrt(np.sum(abs_residuals**2, axis=1))
-    #     return np.quantile(norm, [0.5, 0.95])
-
-
-    # def phase_shift_metric(self,
-    #     filename: str
-    # ):
-    #     with open(filename, 'rb') as f:
-    #         benchmark_data = pickle.load(f)
-        
-    #     thetas = np.array([bd.theta for bd in benchmark_data])
-
-    #     wave_functions = np.array([bd.phi for bd in benchmark_data])
-    #     phase_shifts = np.array([phase_shift_interp(u, self.s_mesh, self.l, self.s_0) for u in wave_functions])
-
-    #     emulated_phase_shifts = np.array([self.emulate_phase_shift(theta) for theta in thetas])
-
-    #     rel_diff = np.abs((emulated_phase_shifts - phase_shifts) / emulated_phase_shifts)
-    #     med, upper = np.quantile(rel_diff, [0.5, 0.95])
-    #     return [med, upper, np.max(np.abs(rel_diff))]
-
-
-    # def run_metrics(self,
-    #     filename: str,
-    #     verbose: bool = False
-    # ):
-    #     wave_function_results = self.wave_function_metric(filename)
-    #     phase_shift_results = self.phase_shift_metric(filename)
-
-    #     if verbose:
-    #         print('Wave function residuals (root of sum of squares):\n50% and 95% quantiles')
-    #         print(f'{wave_function_results[0]:.4e}  {wave_function_results[1]:.4e}')
-    #         print('Phase shift residuals (relative difference):\n50% and 95% quantiles')
-    #         print(f'{phase_shift_results[0]:.4e}  {phase_shift_results[1]:.4e}')
-    #         print('Maximum phase shift residuals')
-    #         print(f'{phase_shift_results[2]:.4e}')
-
-    #     return wave_function_results, phase_shift_results

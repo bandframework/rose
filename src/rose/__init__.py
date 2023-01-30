3b151c747bc2452a6fc2bab580bed1df2f025e88
Daniel Odell <dmcodelljr@gmail.com>
2023-01-30 15:27:26 -0500

    Expose ScatteringAmplitudeEmulator.

--- src/rose/__init__.py
+++ src/rose/__init__.py
@@ -3,4 +3,5 @@ from .interaction_eim import InteractionEIM, Optical_Potential
 from .schroedinger import SchroedingerEquation
 from . import constants, metrics
 from .reduced_basis_emulator import ReducedBasisEmulator
+from .scattering_amplitude_emulator import ScatteringAmplitudeEmulator
 from .free_solutions import phase_shift

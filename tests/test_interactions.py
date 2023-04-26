9eccab05b3b36071e34f89545f995603ffd9f874
Daniel Odell <dmcodelljr@gmail.com>
2023-04-26 11:36:20 -0400

    Updates tests.

--- tests/test_interactions.py
+++ tests/test_interactions.py
@@ -11,9 +11,9 @@ class TestMNPotential(unittest.TestCase):
     def test_mn_potential(self):
         self.assertTrue(
             relative_difference(
-                roqse.MN_Potential.evaluate(1.0, np.array([1.0, 1.0])),
+                rose.MN_Potential.v_r(1.0, np.array([1.0, 1.0])),
                 0.854184893887586) < 1e-12,
-            msg=f'V_MN = {roqse.MN_Potential.evaluate(1.0, np.array([1.0, 1.0]))}')
+            msg=f'V_MN = {rose.MN_Potential.v_r(1.0, np.array([1.0, 1.0]))}')
 
 
 if __name__ == '__main__':
--- tests/test_schroedinger_equation.py
+++ tests/test_schroedinger_equation.py
@@ -11,14 +11,39 @@ _, u_pg = np.loadtxt('u_mn_test.txt', unpack=True)
 u_pg /= np.max(u_pg) # This is a very hacky way to handle normalization.
 
 class TestSchrEq(unittest.TestCase):
-    def test_schrodinger_equation(self):
-        se = roqse.SchroedingerEquation(roqse.MN_Potential)
-        solution = se.solve_se(ENERGY, np.array([V0R, V0S]), r_min=0.01)
-        s = solution[:, 0]
-        u = solution[:, 1] / np.max(solution[:, 1])
+    # def test_schrodinger_equation(self):
+    #     se = roqse.SchroedingerEquation(roqse.MN_Potential)
+    #     solution = se.solve_se(ENERGY, np.array([V0R, V0S]), r_min=0.01)
+    #     s = solution[:, 0]
+    #     u = solution[:, 1] / np.max(solution[:, 1])
+    #     self.assertTrue(
+    #         np.linalg.norm(u - u_pg) < 0.1
+    #     )
+    
+
+    def test_energy(self):
+        mu = 1.0
+        energy = 50.0
+        ell = 0
+        rho = rose.constants.DEFAULT_RHO_MESH
+        potential = lambda r, theta: theta[0] * np.exp(-(r/theta[1])**2) 
+
+        interaction = rose.Interaction(potential, 2, mu, energy, 1, 1)
+        energized_interaction = rose.interaction.EnergizedInteraction(potential, 3, mu, 1, 1)
+
+        se1 = rose.SchroedingerEquation(interaction)
+        se2 = rose.SchroedingerEquation(energized_interaction)
+
+        theta1 = np.array([-10.0, 1.0])
+        theta2 = np.hstack((energy, theta1))
+
+        phi1 = se1.phi(theta1, rho, ell)
+        phi2 = se2.phi(theta2, rho, ell)
+        norm_diff = np.linalg.norm(phi1 - phi2)
 
         self.assertTrue(
-            np.linalg.norm(u - u_pg) < 0.1
+            norm_diff < 1e-16,
+            msg=f'norm(difference) = {norm_diff:.2e}'
         )
 
 

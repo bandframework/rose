c45c5c3642bdbf9490cb9483b27e4a0f8ca3ecf7
Daniel Odell <dmcodelljr@gmail.com>
2023-06-14 11:10:14 -0400

    Expose SpinOrbitTerm.

--- src/rose/__init__.py
+++ src/rose/__init__.py
@@ -7,6 +7,7 @@ from .scattering_amplitude_emulator import ScatteringAmplitudeEmulator
 from .free_solutions import phase_shift
 from .basis import CustomBasis, RelativeBasis
 from .koning_delaroche import KoningDelaroche, EnergizedKoningDelaroche
+from .spin_orbit import SpinOrbitTerm
 
 
 __version__ = '0.9.1'

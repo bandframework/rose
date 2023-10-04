bfe3fc0377b821c9ef12cae3966f187bfd339c3e
beykyle <beykyle@umich.edu>
2023-10-04 12:34:43 -0400

    formatting

--- src/rose/koning_delaroche.py
+++ src/rose/koning_delaroche.py
@@ -1,11 +1,11 @@
-'''The Koning-Delaroche potential is a common optical potential for nuclear
+"""The Koning-Delaroche potential is a common optical potential for nuclear
 scattering. It is provided here in simplified form specifically to address this
 need.
 
 See the [Koning-Delaroche
 paper](https://www.sciencedirect.com/science/article/pii/S0375947402013210) for
 details. Equation references are with respect to (w.r.t.) this paper.
-'''
+"""
 from pathlib import Path
 
 import json
@@ -16,144 +16,152 @@ from .energized_interaction_eim import EnergizedInteractionEIMSpace
 from .constants import DEFAULT_RHO_MESH, MASS_PION, HBARC, ALPHA
 from .utility import kinematics, Projectile
 
-MAX_ARG = np.log(1/1e-16)
+MAX_ARG = np.log(1 / 1e-16)
 NUM_PARAMS = 15
 
+
 def Vv(E, v1, v2, v3, v4, Ef):
-    r'''energy-dependent, volume-central strength - real term, Eq. (7)
-    '''
-    return v1 * (1 - v2*(E-Ef) + v3*(E-Ef)**2 - v4*(E-Ef)**3)
+    r"""energy-dependent, volume-central strength - real term, Eq. (7)"""
+    return v1 * (1 - v2 * (E - Ef) + v3 * (E - Ef) ** 2 - v4 * (E - Ef) ** 3)
 
 
 def Wv(E, w1, w2, Ef):
-    '''energy-dependent, volume-central strength - imaginary term, Eq. (7)
-    '''
-    return w1 * (E-Ef)**2 / ((E-Ef)**2 + w2**2)
+    """energy-dependent, volume-central strength - imaginary term, Eq. (7)"""
+    return w1 * (E - Ef) ** 2 / ((E - Ef) ** 2 + w2**2)
 
 
 def Wd(E, d1, d2, d3, Ef):
-    '''energy-dependent, surface-central strength - imaginary term (no real
-        term), Eq. (7)
-    '''
-    return d1 * (E-Ef)**2 / ((E-Ef)**2 + d3**2) * np.exp(-d2*(E-Ef))
+    """energy-dependent, surface-central strength - imaginary term (no real
+    term), Eq. (7)
+    """
+    return d1 * (E - Ef) ** 2 / ((E - Ef) ** 2 + d3**2) * np.exp(-d2 * (E - Ef))
 
 
 def Vso(E, vso1, vso2, E_f):
-    '''energy-dependent, spin-orbit strength --- real term, Eq. (7)'''
-    return vso1 * np.exp(-vso2*(E-E_f))
+    """energy-dependent, spin-orbit strength --- real term, Eq. (7)"""
+    return vso1 * np.exp(-vso2 * (E - E_f))
 
 
 def Wso(E, wso1, wso2, E_f):
-    '''energy-dependent, spin-orbit strength --- imaginary term, Eq. (7)'''
-    return wso1 * (E - E_f)**2 / ((E - E_f)**2 + wso2**2)
+    """energy-dependent, spin-orbit strength --- imaginary term, Eq. (7)"""
+    return wso1 * (E - E_f) ** 2 / ((E - E_f) ** 2 + wso2**2)
 
 
 def woods_saxon(r, R, a):
-    '''Woods-Saxon potential'''
-    return 1/(1 + np.exp((r-R)/a))
+    """Woods-Saxon potential"""
+    return 1 / (1 + np.exp((r - R) / a))
 
 
 def woods_saxon_safe(r, R, a):
-    '''Woods-Saxon potential
-
-     * avoids `exp` overflows
-
-     '''
-    x = (r-R)/a
-    if isinstance(x, float):
-        return 1/(1 + np.exp(x)) if x < MAX_ARG else 0
-    else:
-        ii = np.where(x <= MAX_ARG)[0]
-        jj = np.where(x > MAX_ARG)[0]
-        return np.hstack([1/(1 + np.exp(x[ii])), np.zeros(jj.size)])
-    
-
-def woods_saxon_prime(r, R, a):
-    '''derivative of the Woods-Saxon potential w.r.t. $r$
-    '''
-    return -1/a * np.exp((r-R)/a) / (1 + np.exp((r-R)/a))**2
-
-
-def woods_saxon_prime_safe(r, R, a):
-    '''derivative of the Woods-Saxon potential w.r.t. $r$
+    """Woods-Saxon potential
 
     * avoids `exp` overflows
 
-    '''
-    x = (r-R)/a
+    """
+    x = (r - R) / a
     if isinstance(x, float):
-        return -1/a * np.exp(x)/(1 + np.exp(x))**2 if x < MAX_ARG else 0
+        return 1 / (1 + np.exp(x)) if x < MAX_ARG else 0
     else:
         ii = np.where(x <= MAX_ARG)[0]
         jj = np.where(x > MAX_ARG)[0]
-        return np.hstack([-1/a * np.exp(x[ii])/(1 + np.exp(x[ii]))**2, np.zeros(jj.size)])
+        return np.hstack([1 / (1 + np.exp(x[ii])), np.zeros(jj.size)])
+
+
+def woods_saxon_prime(r, R, a):
+    """derivative of the Woods-Saxon potential w.r.t. $r$"""
+    return -1 / a * np.exp((r - R) / a) / (1 + np.exp((r - R) / a)) ** 2
+
+
+def woods_saxon_prime_safe(r, R, a):
+    """derivative of the Woods-Saxon potential w.r.t. $r$
+
+    * avoids `exp` overflows
+
+    """
+    x = (r - R) / a
+    if isinstance(x, float):
+        return -1 / a * np.exp(x) / (1 + np.exp(x)) ** 2 if x < MAX_ARG else 0
+    else:
+        ii = np.where(x <= MAX_ARG)[0]
+        jj = np.where(x > MAX_ARG)[0]
+        return np.hstack(
+            [-1 / a * np.exp(x[ii]) / (1 + np.exp(x[ii])) ** 2, np.zeros(jj.size)]
+        )
 
 
 def KD(r, E, v1, v2, v3, v4, w1, w2, d1, d2, d3, Ef, Rv, av, Rd, ad):
-    '''Koning-Delaroche without the spin-orbit terms - Eq. (1)'''
-    return -Vv(E, v1, v2, v3, v4, Ef) * woods_saxon(r, Rv, av) - \
-           1j*Wv(E, w1, w2, Ef) * woods_saxon(r, Rv, av) - \
-           1j * (-4*ad) * Wd(E, d1, d2, d3, Ef) * woods_saxon_prime(r, Rd, ad)
+    """Koning-Delaroche without the spin-orbit terms - Eq. (1)"""
+    return (
+        -Vv(E, v1, v2, v3, v4, Ef) * woods_saxon(r, Rv, av)
+        - 1j * Wv(E, w1, w2, Ef) * woods_saxon(r, Rv, av)
+        - 1j * (-4 * ad) * Wd(E, d1, d2, d3, Ef) * woods_saxon_prime(r, Rd, ad)
+    )
 
 
 def decompose_alpha(alpha):
-    r'''Splits the parameter-space vector into non-spin-orbit and spin-orbit
+    r"""Splits the parameter-space vector into non-spin-orbit and spin-orbit
     parameters.
 
     Parameters:
         alpha (ndarray): interaction parameters
-    
+
     Returns:
         parameters (tuple): 2-tuple of non-spin-orbit (`parameters[0]`) and
             spin-orbit parameters (`parameters[1]`)
 
-    '''
+    """
     vv, rv, av, wv, rwv, awv, wd, rd, ad, vso, rso, aso, wso, rwso, awso = alpha
     return (vv, rv, av, wv, rwv, awv, wd, rd, ad), (vso, rso, aso, wso, rwso, awso)
 
+
 def KD_simple(r, alpha):
-    r'''simplified Koning-Delaroche without the spin-orbit terms
+    r"""simplified Koning-Delaroche without the spin-orbit terms
 
     Take Eq. (1) and remove the energy dependence of the coefficients.
-    '''
+    """
     vv, rv, av, wv, rwv, awv, wd, rd, ad = decompose_alpha(alpha)[0]
-    return -vv * woods_saxon_safe(r, rv, av) - \
-        1j*wv * woods_saxon_safe(r, rwv, awv) - \
-        1j*(-4*ad)*wd * woods_saxon_prime_safe(r, rd, ad)
+    return (
+        -vv * woods_saxon_safe(r, rv, av)
+        - 1j * wv * woods_saxon_safe(r, rwv, awv)
+        - 1j * (-4 * ad) * wd * woods_saxon_prime_safe(r, rd, ad)
+    )
 
 
 def KD_simple_so(r, alpha, lds):
-    r'''simplified Koning-Delaroche spin-orbit terms
+    r"""simplified Koning-Delaroche spin-orbit terms
 
     Take Eq. (1) and remove the energy dependence of the coefficients.
 
     lds: l • s = 1/2 * (j(j+1) - l(l+1) - s(s+1))
-    '''
+    """
     vso, rso, aso, wso, rwso, awso = decompose_alpha(alpha)[1]
-    return lds * vso/MASS_PION**2/r * woods_saxon_prime_safe(r, rso, aso) + \
-        1j*wso/MASS_PION**2/r*woods_saxon_prime_safe(r, rwso, awso)
+    return lds * vso / MASS_PION**2 / r * woods_saxon_prime_safe(
+        r, rso, aso
+    ) + 1j * wso / MASS_PION**2 / r * woods_saxon_prime_safe(r, rwso, awso)
 
 
 class KoningDelaroche(InteractionEIMSpace):
-    r'''Koning-Delaroche potential (without energy-dependent strength
+    r"""Koning-Delaroche potential (without energy-dependent strength
     coefficients) for arbitrary systems defined by `mu`, `energy`, `ell`, `Z_1`,
     and `Z_2`.
-    '''
-    def __init__(self,
+    """
+
+    def __init__(
+        self,
         mu: float,
         ell: int,
         energy: float,
         training_info: np.array,
-        l_max = 20,
+        l_max=20,
         n_basis: int = 8,
         explicit_training: bool = False,
         n_train: int = 1000,
         rho_mesh: np.array = DEFAULT_RHO_MESH,
-        match_points: np.array = None
+        match_points: np.array = None,
     ):
-        r'''Wraps the Koning-Delaroche potential into a `rose`-friendly class.
+        r"""Wraps the Koning-Delaroche potential into a `rose`-friendly class.
         Saves system-specific information.
-        
+
         Parameters:
             mu (float): reduced mass of the 2-body system
             ell (int): angular momentum
@@ -178,34 +186,45 @@ class KoningDelaroche(InteractionEIMSpace):
         Returns:
             instance (KoningDelaroche): instance of the class
 
-        '''
+        """
         super().__init__(
-            KD_simple_so, NUM_PARAMS, mu, ell, energy, training_info=training_info, Z_1=0, Z_2=0,
-            l_max = lmax,
-            is_complex=True, n_basis=n_basis,
-            explicit_training=explicit_training, n_train=n_train,
-            rho_mesh=rho_mesh, match_points=match_points
+            KD_simple_so,
+            NUM_PARAMS,
+            mu,
+            ell,
+            energy,
+            training_info=training_info,
+            Z_1=0,
+            Z_2=0,
+            l_max=lmax,
+            is_complex=True,
+            n_basis=n_basis,
+            explicit_training=explicit_training,
+            n_train=n_train,
+            rho_mesh=rho_mesh,
+            match_points=match_points,
         )
 
 
 class EnergizedKoningDelaroche(EnergizedInteractionEIMSpace):
-    def __init__(self,
+    def __init__(
+        self,
         mu: float,
         ell: int,
         training_info: np.array,
-        l_max = 20,
+        l_max=20,
         n_basis: int = 8,
         explicit_training: bool = False,
         n_train: int = 1000,
         rho_mesh: np.array = DEFAULT_RHO_MESH,
-        match_points: np.array = None
+        match_points: np.array = None,
     ):
-        r'''Wraps the Koning-Delaroche potential into a `rose`-friendly class.
+        r"""Wraps the Koning-Delaroche potential into a `rose`-friendly class.
         Saves system-specific information. Allows the user to emulate across
         energies.
 
         * **Does not (yet) support Coulomb.**
-        
+
         Parameters:
             mu (float): reduced mass of the 2-body system
             ell (int): angular momentum
@@ -228,35 +247,47 @@ class EnergizedKoningDelaroche(EnergizedInteractionEIMSpace):
 
         Returns:
             instance (EnergizedKoningDelaroche): instance of the class
-        '''
+        """
         super().__init__(
-            KD_simple_so, NUM_PARAMS, mu, ell, training_info=training_info, Z_1=0, Z_2=0,
-            l_max = lmax,
-            is_complex=True, n_basis=n_basis,
-            explicit_training=explicit_training, n_train=n_train,
-            rho_mesh=rho_mesh, match_points=match_points
+            KD_simple_so,
+            NUM_PARAMS,
+            mu,
+            ell,
+            training_info=training_info,
+            Z_1=0,
+            Z_2=0,
+            l_max=lmax,
+            is_complex=True,
+            n_basis=n_basis,
+            explicit_training=explicit_training,
+            n_train=n_train,
+            rho_mesh=rho_mesh,
+            match_points=match_points,
         )
 
 
-
-class KDGlobal():
+class KDGlobal:
     r"""Global optical potential in Koning-Delaroche form."""
 
-    def __init__(self, projectile : Projectile, param_fpath: Path = None):
-        r'''
+    def __init__(self, projectile: Projectile, param_fpath: Path = None):
+        r"""
         Parameters:
             projectile : neutron or proton?
             param_fpath : path to json file encoding parameter values. Defaults to data/KD_default.json
-        '''
+        """
         if param_fpath is None:
-            param_fpath = Path(__file__).parent.resolve() /  Path("../../data/KD_default.json")
+            param_fpath = Path(__file__).parent.resolve() / Path(
+                "../../data/KD_default.json"
+            )
 
         if projectile == Projectile.neutron:
             tag = "_n"
         elif projectile == Projectile.proton:
             tag = "_p"
         else:
-            raise RuntimeError("KDGlobal is defined only for neutron and proton projectiles")
+            raise RuntimeError(
+                "KDGlobal is defined only for neutron and proton projectiles"
+            )
 
         self.projectile = projectile
 
@@ -283,8 +314,8 @@ class KDGlobal():
             # imag volume depth
             self.w1_0 = data["KDImagVolume_W1_0" + tag]
             self.w1_A = data["KDImagVolume_W1_A" + tag]
-            self.w2_0 = data["KDImagVolume_W2_0" ]
-            self.w2_A = data["KDImagVolume_W2_A" ]
+            self.w2_0 = data["KDImagVolume_W2_0"]
+            self.w2_A = data["KDImagVolume_W2_A"]
 
             # imag surface depth
             self.d1_0 = data["KDImagSurface_D1_0"]
@@ -329,8 +360,6 @@ class KDGlobal():
                 self.rc_A = data["KDCoulomb_r_C_A"]
                 self.rc_A2 = data["KDCoulomb_r_C_A2"]
 
-
-
     def get_params(self, A, Z, Elab):
         """
         Calculates Koning-Delaroche global neutron-nucleus OMP parameters for given A, Z,
@@ -338,9 +367,9 @@ class KDGlobal():
         """
 
         if self.projectile == Projectile.neutron:
-            projectile = (1,0)
+            projectile = (1, 0)
         elif self.projectile == Projectile.proton:
-            projectile = (1,1)
+            projectile = (1, 1)
 
         mu, Ecom, k = kinematics((A, Z), projectile, Elab)
         eta = 0
@@ -352,7 +381,7 @@ class KDGlobal():
         delta = (N - Z) / A
         factor = 1
         if self.projectile == Projectile.proton:
-            delta *= -1.
+            delta *= -1.0
             factor *= -1
 
         # fermi energy
@@ -366,7 +395,7 @@ class KDGlobal():
         vv = Vv(Ecom, v1, v2, v3, v4, Ef)
 
         # real central form
-        rv = self.rv_0 - self.rv_A * A ** (-1. / 3.)
+        rv = self.rv_0 - self.rv_A * A ** (-1.0 / 3.0)
         av = self.av_0 - self.av_A * A
 
         # imag volume depth
@@ -385,7 +414,7 @@ class KDGlobal():
         wd = Wd(Ecom, d1, d2, d3, Ef)
 
         # imag surface form
-        rd = self.rd_0 - self.rd_A * A ** (1. / 3.)
+        rd = self.rd_0 - self.rd_A * A ** (1.0 / 3.0)
         ad = self.ad_0 - self.ad_A * A * factor
 
         # real spin orbit depth
@@ -394,7 +423,7 @@ class KDGlobal():
         vso = Vso(Ecom, vso1, vso2, Ef)
 
         # real spin orbit form
-        rso = self.rso_0 - self.rso_A * A ** (-1. / 3.)
+        rso = self.rso_0 - self.rso_A * A ** (-1.0 / 3.0)
         aso = self.aso_0
 
         # imag spin orbit form
@@ -409,29 +438,35 @@ class KDGlobal():
         # Coulomb radius
         R_C = 0
         if self.projectile == Projectile.proton:
-            rc0 = self.rc_0 + self.rc_A * A**(-2./3.) + self.rc_A2 * A**(-5./3.)
-            R_C = rc0 * A **(1./3.)
+            rc0 = (
+                self.rc_0
+                + self.rc_A * A ** (-2.0 / 3.0)
+                + self.rc_A2 * A ** (-5.0 / 3.0)
+            )
+            R_C = rc0 * A ** (1.0 / 3.0)
 
         # 15 params total
         params = np.array(
             [
                 vv,
-                rv * A ** (1. / 3.),
+                rv * A ** (1.0 / 3.0),
                 av,
                 wv,
-                rwv * A ** (1. / 3.),
+                rwv * A ** (1.0 / 3.0),
                 awv,
                 wd,
-                rd * A ** (1. / 3.),
+                rd * A ** (1.0 / 3.0),
                 ad,
                 vso,
-                rso * A ** (1. / 3.),
+                rso * A ** (1.0 / 3.0),
                 aso,
                 wso,
-                rwso * A ** (1. / 3.),
+                rwso * A ** (1.0 / 3.0),
                 awso,
             ]
         )
 
-        return (mu, Ecom, k, eta, R_C), params,
-
+        return (
+            (mu, Ecom, k, eta, R_C),
+            params,
+        )

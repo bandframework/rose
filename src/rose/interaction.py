2fdb2315ef7147f22fcbd31488c214b67b9d3788
beykyle <beykyle@umich.edu>
2023-10-10 22:06:45 -0400

    formatting

--- src/rose/interaction.py
+++ src/rose/interaction.py
@@ -1,28 +1,30 @@
-'''Wraps the user-defined interaction into a class that stores several relevant
+"""Wraps the user-defined interaction into a class that stores several relevant
 parameters of the problem.
-'''
+"""
 
 from typing import Callable
 import numpy as np
 from .constants import HBARC, ALPHA
 from .spin_orbit import SpinOrbitTerm
 
+
 class Interaction:
-    '''Defines a local, (possibly) complex, affine, fixed-energy interaction.
-    '''
-    def __init__(self,
-        coordinate_space_potential: Callable[[float, np.array], float], # V(r, theta)
-        n_theta: int, # How many parameters does the interaction have?
-        mu: float, # reduced mass (MeV)
-        energy: float, # E_{c.m.}
+    """Defines a local, (possibly) complex, affine, fixed-energy interaction."""
+
+    def __init__(
+        self,
+        coordinate_space_potential: Callable[[float, np.array], float],  # V(r, theta)
+        n_theta: int,  # How many parameters does the interaction have?
+        mu: float,  # reduced mass (MeV)
+        energy: float,  # E_{c.m.}
         ell: int,
-        Z_1: int = 0, # atomic number of particle 1
-        Z_2: int = 0, # atomic number of particle 2
-        R_C: float = 0.0, # Coulomb "cutoff"
+        Z_1: int = 0,  # atomic number of particle 1
+        Z_2: int = 0,  # atomic number of particle 2
+        R_C: float = 0.0,  # Coulomb "cutoff"
         is_complex: bool = False,
         spin_orbit_term: SpinOrbitTerm = None,
     ):
-        r'''Creates a local, (possibly) complex, affine, fixed-energy interaction.
+        r"""Creates a local, (possibly) complex, affine, fixed-energy interaction.
 
         Parameters:
             coordinate_space_potential (Callable[[float,ndarray],float]): V(r, theta)
@@ -35,7 +37,7 @@ class Interaction:
             R_C (float): Coulomb "cutoff" radius
             is_complex (bool): Is the interaction complex?
             spin_orbit_term (SpinOrbitTerm): See [Spin-Orbit section](#spin-orbit).
-        
+
         Returns:
             instance (Interaction): instance of `Interaction`
 
@@ -48,12 +50,12 @@ class Interaction:
             is_complex (bool): Is this a complex potential?
             spin_orbit_term (SpinOrbitTerm): See [Spin-Orbit section](#spin-orbit)
 
-        '''
+        """
         self.v_r = coordinate_space_potential
         self.n_theta = n_theta
-        self.mu = mu / HBARC # Go ahead and convert to 1/fm
+        self.mu = mu / HBARC  # Go ahead and convert to 1/fm
         self.ell = ell
-        self.k_c = ALPHA * Z_1*Z_2 * self.mu
+        self.k_c = ALPHA * Z_1 * Z_2 * self.mu
         self.R_C = R_C
         self.is_complex = is_complex
         self.spin_orbit_term = spin_orbit_term
@@ -67,7 +69,7 @@ class Interaction:
             # If the energy is specified (not None as it is when subclass
             # EnergizedInteraction instantiates), set up associated attributes.
             self.energy = energy
-            self.k = np.sqrt(2 * self.mu * self.energy/HBARC)
+            self.k = np.sqrt(2 * self.mu * self.energy / HBARC)
             self.sommerfeld = self.k_c / self.k
         else:
             # If the energy is not specified, these will be set up when the
@@ -76,12 +78,8 @@ class Interaction:
             self.k = None
             self.sommerfeld = None
 
-
-    def tilde(self,
-        s: float,
-        alpha: np.array
-    ):
-        r'''Scaled potential, $\tilde{U}(s, \alpha, E)$.
+    def tilde(self, s: float, alpha: np.array):
+        r"""Scaled potential, $\tilde{U}(s, \alpha, E)$.
 
         * Does not include the Coulomb term.
         * $E = E_{c.m.}$; `[E] = MeV = [v_r]`
@@ -89,23 +87,24 @@ class Interaction:
         Parameters:
             s (float): mesh point; $s = pr/\hbar$
             alpha (ndarray): the varied parameters
-        
+
         Returns:
             u_tilde (float | complex): value of scaled interaction
 
-        '''
-        vr = self.v_r(s/self.k, alpha)
-        vr += self.spin_orbit_term.spin_orbit_potential(s/self.k, alpha) if self.include_spin_orbit else 0
-        return  1.0/self.energy * vr
+        """
+        vr = self.v_r(s / self.k, alpha)
+        vr += (
+            self.spin_orbit_term.spin_orbit_potential(s / self.k, alpha)
+            if self.include_spin_orbit
+            else 0
+        )
+        return 1.0 / self.energy * vr
 
-
-    def basis_functions(self,
-        rho_mesh: np.array
-    ):
-        r'''In general, we approximate the potential as
+    def basis_functions(self, rho_mesh: np.array):
+        r"""In general, we approximate the potential as
 
         $\hat{U} = \sum_{j} \beta_j(\alpha) u_j$
-        
+
         For affine interactions (like those defined in this class) the basis
         functions (or "pillars), $u_j$, are just the "naked" parts of the
         potential. As seen below, it is assumed that the $\beta_j(\alpha)$
@@ -114,36 +113,28 @@ class Interaction:
         Parameters:
             rho_mesh (ndarray): discrete $\rho$ values at which the potential is
                 going to be evaluated
-        
+
         Returns:
             value (ndarray): values of the scaled potential at provided $\rho$ points
-        '''
-        return np.array([
-            self.tilde(rho_mesh, row) for row in np.eye(self.n_theta)
-        ]).T
-    
+        """
+        return np.array([self.tilde(rho_mesh, row) for row in np.eye(self.n_theta)]).T
 
-    def coefficients(self,
-        alpha: np.array # interaction parameters
-    ):
-        r'''As noted in the `basis_functions` documentation, the coefficients
+    def coefficients(self, alpha: np.array):  # interaction parameters
+        r"""As noted in the `basis_functions` documentation, the coefficients
         for affine interactions are simply the parameter values. The inverse of
         the momentum is also returned to support energy emulation.
 
         Parameters:
             alpha (ndarray): parameter point
-        
+
         Returns:
             result (tuple): inverse momentum and coefficients
 
-        '''
-        return 1/self.k, alpha
-    
+        """
+        return 1 / self.k, alpha
 
-    def eta(self,
-        alpha: np.array
-    ):
-        r'''Sommerfeld parameter. Implemented as a function to support energy
+    def eta(self, alpha: np.array):
+        r"""Sommerfeld parameter. Implemented as a function to support energy
         emulation (where the energy could be a part of the parameter vector,
         `alpha`).
 
@@ -152,12 +143,11 @@ class Interaction:
 
         Returns:
             eta (float): Sommerfeld parametere
-        '''
+        """
         return self.sommerfeld
 
-
     def momentum(self, alpha: np.array):
-        r'''Momentum. Implemented as a function to support energy emulation
+        r"""Momentum. Implemented as a function to support energy emulation
         (where the energy/momentum could be a part of the parameter vector,
         `alpha`).
 
@@ -166,11 +156,11 @@ class Interaction:
 
         Returns:
             k (float): center-of-mass, scattering momentum
-        '''
+        """
         return self.k
 
-    def coulomb_cutoff(self, alpha : np.array):
-        r'''Coulomb cutoff. Implemented as a function to support energy emulation
+    def coulomb_cutoff(self, alpha: np.array):
+        r"""Coulomb cutoff. Implemented as a function to support energy emulation
         (where the energy/momentum could be a part of the parameter vector,
         `alpha`).
 
@@ -179,25 +169,28 @@ class Interaction:
 
         Returns:
             R_C (float): Coulomb cutoff
-        '''
+        """
         return self.R_C
 
 
 class InteractionSpace:
-    def __init__(self,
-        coordinate_space_potential: Callable[[float, np.array], float], # V(r, theta)
-        n_theta: int, # How many parameters does the interaction have?
-        mu: float, # reduced mass (MeV)
-        energy: float, # E_{c.m.}
-        l_max: int,
-        Z_1: int = 0, # atomic number of particle 1
-        Z_2: int = 0, # atomic number of particle 2
+    def __init__(
+        self,
+        coordinate_space_potential: Callable[[float, np.array], float],  # V(r, theta)
+        n_theta: int,  # How many parameters does the interaction have?
+        mu: float,  # reduced mass (MeV)
+        energy: float,  # E_{c.m.}
+        l_max: int = 20,
+        Z_1: int = 0,  # atomic number of particle 1
+        Z_2: int = 0,  # atomic number of particle 2
         R_C: float = 0.0,
         is_complex: bool = False,
-        spin_orbit_potential: Callable[[float, np.array, float], float] = None #V_{SO}(r, theta, l•s)
+        spin_orbit_potential: Callable[
+            [float, np.array, float], float
+        ] = None,  # V_{SO}(r, theta, l•s)
     ):
-        r'''Generates a list of $\ell$-specific interactions.
-        
+        r"""Generates a list of $\ell$-specific interactions.
+
         Parameters:
             coordinate_space_potential (Callable[[float,ndarray],float]): V(r, theta)
             n_theta (int): number of parameters
@@ -215,39 +208,58 @@ class InteractionSpace:
 
         Attributes:
             interaction (list): list of `Interaction`s
-        '''
+        """
         self.l_max = l_max
         self.interactions = []
         if spin_orbit_potential is None:
-            for l in range(l_max+1):
+            for l in range(l_max + 1):
                 self.interactions.append(
-                    [Interaction(coordinate_space_potential, n_theta, mu,
-                        energy, l, Z_1=Z_1, Z_2=Z_2, R_C=R_C,
-                        is_complex=is_complex)]
+                    [
+                        Interaction(
+                            coordinate_space_potential,
+                            n_theta,
+                            mu,
+                            energy,
+                            l,
+                            Z_1=Z_1,
+                            Z_2=Z_2,
+                            R_C=R_C,
+                            is_complex=is_complex,
+                        )
+                    ]
                 )
         else:
-            for l in range(l_max+1):
+            for l in range(l_max + 1):
                 self.interactions.append(
-                    [Interaction(coordinate_space_potential, n_theta, mu,
-                        energy, l, Z_1=Z_1, Z_2=Z_2, R_C=R_C,
-                        is_complex=is_complex,
-                        spin_orbit_term=SpinOrbitTerm(spin_orbit_potential,
-                        lds)) for lds in couplings(l)]
+                    [
+                        Interaction(
+                            coordinate_space_potential,
+                            n_theta,
+                            mu,
+                            energy,
+                            l,
+                            Z_1=Z_1,
+                            Z_2=Z_2,
+                            R_C=R_C,
+                            is_complex=is_complex,
+                            spin_orbit_term=SpinOrbitTerm(spin_orbit_potential, lds),
+                        )
+                        for lds in couplings(l)
+                    ]
                 )
 
 
 def couplings(l):
-    r'''For a spin-1/2 nucleon scattering off a spin-0 nucleus, there are
+    r"""For a spin-1/2 nucleon scattering off a spin-0 nucleus, there are
     maximally 2 different total angular momentum couplings: l+1/2 and l-1/2.
-    
+
     Parameters:
         l (int): angular momentum
-    
+
     Returns:
         couplings (list): total angular momentum possibilities
-    '''
+    """
     if l == 0:
         return [l]
     else:
-        return [l, -(l+1)]
-
+        return [l, -(l + 1)]

c7a824c984fc246f3c02ad27e12c330173d2e724
beykyle <beykyle@umich.edu>
2023-10-11 17:06:12 -0400

    njit what we can

--- src/rose/koning_delaroche.py
+++ src/rose/koning_delaroche.py
@@ -10,6 +10,7 @@ from pathlib import Path
 
 import json
 import numpy as np
+from numba import njit
 
 from .interaction_eim import InteractionEIMSpace
 from .energized_interaction_eim import EnergizedInteractionEIMSpace
@@ -19,17 +20,19 @@ from .utility import kinematics, Projectile
 MAX_ARG = np.log(1 / 1e-16)
 NUM_PARAMS = 15
 
-
+@njit
 def Vv(E, v1, v2, v3, v4, Ef):
     r"""energy-dependent, volume-central strength - real term, Eq. (7)"""
     return v1 * (1 - v2 * (E - Ef) + v3 * (E - Ef) ** 2 - v4 * (E - Ef) ** 3)
 
 
+@njit
 def Wv(E, w1, w2, Ef):
     """energy-dependent, volume-central strength - imaginary term, Eq. (7)"""
     return w1 * (E - Ef) ** 2 / ((E - Ef) ** 2 + w2**2)
 
 
+@njit
 def Wd(E, d1, d2, d3, Ef):
     """energy-dependent, surface-central strength - imaginary term (no real
     term), Eq. (7)
@@ -37,21 +40,25 @@ def Wd(E, d1, d2, d3, Ef):
     return d1 * (E - Ef) ** 2 / ((E - Ef) ** 2 + d3**2) * np.exp(-d2 * (E - Ef))
 
 
+@njit
 def Vso(E, vso1, vso2, E_f):
     """energy-dependent, spin-orbit strength --- real term, Eq. (7)"""
     return vso1 * np.exp(-vso2 * (E - E_f))
 
 
+@njit
 def Wso(E, wso1, wso2, E_f):
     """energy-dependent, spin-orbit strength --- imaginary term, Eq. (7)"""
     return wso1 * (E - E_f) ** 2 / ((E - E_f) ** 2 + wso2**2)
 
 
+@njit
 def woods_saxon(r, R, a):
     """Woods-Saxon potential"""
     return 1 / (1 + np.exp((r - R) / a))
 
 
+@njit
 def woods_saxon_safe(r, R, a):
     """Woods-Saxon potential
 
@@ -64,14 +71,16 @@ def woods_saxon_safe(r, R, a):
     else:
         ii = np.where(x <= MAX_ARG)[0]
         jj = np.where(x > MAX_ARG)[0]
-        return np.hstack([1 / (1 + np.exp(x[ii])), np.zeros(jj.size)])
+        return np.hstack( (1 / (1 + np.exp(x[ii])), np.zeros(jj.size)) )
 
 
+@njit
 def woods_saxon_prime(r, R, a):
     """derivative of the Woods-Saxon potential w.r.t. $r$"""
     return -1 / a * np.exp((r - R) / a) / (1 + np.exp((r - R) / a)) ** 2
 
 
+@njit
 def woods_saxon_prime_safe(r, R, a):
     """derivative of the Woods-Saxon potential w.r.t. $r$
 
@@ -85,10 +94,10 @@ def woods_saxon_prime_safe(r, R, a):
         ii = np.where(x <= MAX_ARG)[0]
         jj = np.where(x > MAX_ARG)[0]
         return np.hstack(
-            [-1 / a * np.exp(x[ii]) / (1 + np.exp(x[ii])) ** 2, np.zeros(jj.size)]
+            (-1 / a * np.exp(x[ii]) / (1 + np.exp(x[ii])) ** 2, np.zeros(jj.size))
         )
 
-
+@njit
 def KD(r, E, v1, v2, v3, v4, w1, w2, d1, d2, d3, Ef, Rv, av, Rd, ad):
     """Koning-Delaroche without the spin-orbit terms - Eq. (1)"""
     return (
@@ -97,7 +106,7 @@ def KD(r, E, v1, v2, v3, v4, w1, w2, d1, d2, d3, Ef, Rv, av, Rd, ad):
         - 1j * (-4 * ad) * Wd(E, d1, d2, d3, Ef) * woods_saxon_prime(r, Rd, ad)
     )
 
-
+@njit
 def decompose_alpha(alpha):
     r"""Splits the parameter-space vector into non-spin-orbit and spin-orbit
     parameters.
@@ -114,6 +123,7 @@ def decompose_alpha(alpha):
     return (vv, rv, av, wv, rwv, awv, wd, rd, ad), (vso, rso, aso, wso, rwso, awso)
 
 
+@njit
 def KD_simple(r, alpha):
     r"""simplified Koning-Delaroche without the spin-orbit terms
 
@@ -126,7 +136,7 @@ def KD_simple(r, alpha):
         - 1j * (-4 * ad) * wd * woods_saxon_prime_safe(r, rd, ad)
     )
 
-
+@njit
 def KD_simple_so(r, alpha, lds):
     r"""simplified Koning-Delaroche spin-orbit terms
 

0c2fdb4935f56f67d1e4c9d0bc960a3fe0e3f376
beykyle <beykyle@umich.edu>
2023-10-30 10:29:58 -0400

    add c

--- src/rose/constants.py
+++ src/rose/constants.py
@@ -8,3 +8,4 @@ MASS_PION = np.sqrt(1 / 2)  # 2.0 per Thomspon&Nuñes below Eq. (4.3.10) 1/fm
 AMU = 931.494102  # MeV/c^2, Particle Data Group
 MASS_N = 1.008665 * AMU  # MeV/c^2 PDG
 MASS_P = 1.007276 * AMU  # MeV/c^2 PDG
+C = 2.99792458e23 #fm/s

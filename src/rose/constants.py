8cadd401687d7f1ce93934efc67bfba667e5e230
beykyle <beykyle@umich.edu>
2023-11-07 10:37:53 -0500

    more precise constants

--- src/rose/constants.py
+++ src/rose/constants.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-HBARC = 197.326
+HBARC = 197.3269804  # hbar*c in [MeV femtometers]
 DEFAULT_RHO_MESH = np.linspace(1e-6, 8 * np.pi, 2000)
 DEFAULT_ANGLE_MESH = np.linspace(0.01, np.pi, 10)
-ALPHA = 0.0072973525693  # 1.0/137.056
+ALPHA = 1.0 / 137.0359991  # dimensionless fine structure constant
 MASS_PION = np.sqrt(1 / 2)  # 2.0 per Thomspon&Nuñes below Eq. (4.3.10) 1/fm
 AMU = 931.494102  # MeV/c^2, Particle Data Group
 MASS_N = 1.008665 * AMU  # MeV/c^2 PDG

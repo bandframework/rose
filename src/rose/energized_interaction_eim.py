add pct expl variance for svd classes, fix EIM inheritance, fix kd_simple_so bug

src/rose/energized_interaction_eim.py
@@ -245,7 +245,7 @@ class EnergizedInteractionEIM(InteractionEIM):
         return (alpha[self.param_mask], k, S_C, E, eta, l, v_r, v_so, l_dot_s)
 
 
-class EnergizedInteractionEIMSpace(InteractionSpace):
+class EnergizedInteractionEIMSpace(InteractionEIMSpace):
     def __init__(
         self,
         coordinate_space_potential: Callable[[float, np.array], float],  # V(r, theta)

src/rose/training.py
@@ -33,7 +33,11 @@ colors = [
 
 
 def sample_params_LHC(
-    N: int, central_vals: np.array = None, scale: float = None, seed: int = None, bounds : np.array = None,
+    N: int,
+    central_vals: np.array = None,
+    scale: float = None,
+    seed: int = None,
+    bounds: np.array = None,
 ):
     r"""
     Sampling parameters from a finite box in parameter space around some central values using the Latin hypercube method

src/rose/utility.py
@@ -23,7 +23,7 @@ class Projectile(Enum):
     proton = 1
 
 
-def latin_hypercube_sample(n_sample : int, bounds : np.array, seed=None):
+def latin_hypercube_sample(n_sample: int, bounds: np.array, seed=None):
     r"""
     Generates N Latin hypercube samples in the k-Dimensional box
     defined by bounds, with the first column being lower and 2nd being upper
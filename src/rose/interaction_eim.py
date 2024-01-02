averaging for timing, fix eim dimensionality issue

src/rose/interaction_eim.py
@@ -82,6 +82,8 @@ class InteractionEIM(Interaction):
             r_i (ndarray): copy of `match_points` (???)
             Ainv (ndarray): inverse of A matrix (Ax = b)
         """
+        if n_basis is None:
+            n_basis = n_theta
 
         super().__init__(
             coordinate_space_potential,
@@ -126,21 +128,16 @@ class InteractionEIM(Interaction):
             self.r_i = rho_mesh[self.match_indices]
             self.Ainv = np.linalg.inv(self.snapshots[self.match_indices])
         elif match_points is None and method == "collocation":
-            if n_basis is None:
-                n_basis = n_theta
             self.snapshots = np.copy(U[:, :n_basis])
             # random r points between 0 and 2π fm
             i_max = self.snapshots.shape[0] // 4
             di = i_max // (n_basis - 1)
             i_init = np.arange(0, i_max + 1, di)
             self.match_indices = max_vol(self.snapshots, i_init)
-            # np.random.randint(0, self.snapshots.shape[0], size=self.snapshots.shape[1]))
             self.match_points = rho_mesh[self.match_indices]
             self.r_i = np.copy(self.match_points)
             self.Ainv = np.linalg.inv(self.snapshots[self.match_indices])
         elif method == "least-squares":
-            if n_basis is None:
-                n_basis = n_theta
             self.snapshots = np.copy(U[:, :n_basis])
             # random r points between 0 and 2π fm
             if match_points is None:

src/rose/training.py
@@ -183,7 +183,12 @@ def build_sae(
                 )
                 new_basis_list[-1].singular_values = basis.singular_values[:n_basis]
             new_bases.append(new_basis_list)
-        emulator = ScatteringAmplitudeEmulator(interactions, new_bases, **SAE_kwargs)
+        emulator = ScatteringAmplitudeEmulator(
+            interactions,
+            new_bases,
+            s_0 = basis.solver.s_0,
+            **SAE_kwargs,
+        )
     else:
         emulator = ScatteringAmplitudeEmulator.from_train(
             interactions,
@@ -281,6 +286,7 @@ class CATPerformance:
         benchmark_inputs: list,
         benchmark_ground_truth: np.array,
         label: str = None,
+        n_timing : int = 1,
     ):
         r"""
         Run benchmark_runner for each of benchmark_inputs, and compare the output
@@ -294,6 +300,7 @@ class CATPerformance:
                 element is an np.array of same shape as output of benchmark_runner, encapsulating
                 the 'exact', or expected, output for a given input
             label : name or identifier for this particular benchmark_runner
+            n_timing : number of times to run benchmark_runner, the average of which is the time
 
         Returns:
             CATPerformance object encapsulating the mean squared error of each output of
@@ -322,9 +329,12 @@ class CATPerformance:
         self.rel_err = np.zeros(all_output_shape, dtype=np.double)
         self.times = np.zeros(self.num_inputs)
         for i in tqdm(range(self.num_inputs)):
-            st = perf_counter()
-            predicted = benchmark_runner(benchmark_inputs[i])
-            et = perf_counter()
+            for j in range(int(n_timing)):
+                st = perf_counter()
+                predicted = benchmark_runner(benchmark_inputs[i])
+                et = perf_counter()
+                self.times[i] += et - st
+            self.times[i] /= n_timing
             self.runner_output[i, ...] = predicted
             self.runner_residuals[i, ...] = benchmark_ground_truth[i] - predicted
             self.rel_err[i, ...] = np.sqrt(
@@ -334,7 +344,6 @@ class CATPerformance:
             ) / np.sqrt(
                 np.real(benchmark_ground_truth[i] * benchmark_ground_truth[i].conj())
             )
-            self.times[i] = et - st
 
         # take median along all axes but 0
         axes = [i + 1 for i in range(len(self.output_shape))]
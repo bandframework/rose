c01eec64df95742cbfb9442f3d82b25f22629124
beykyle <beykyle@umich.edu>
2025-01-15 11:25:28 -0500

    update jitr interface

--- src/rose/lagrangelegendersolver.py
+++ src/rose/lagrangelegendersolver.py
@@ -15,10 +15,8 @@ class LagrangeRmatrix(SchroedingerEquation):
         self,
         interaction: Interaction,
         s_0,
-        solver: jitr.RMatrixSolver,
+        solver: jitr.rmatrix.Solver,
     ):
-        l = np.array([interaction.ell])
-        a = np.array([s_0])
         self.s_0 = s_0
         self.domain = [0, s_0]
         self.interaction = interaction
@@ -26,25 +24,29 @@ class LagrangeRmatrix(SchroedingerEquation):
 
         if self.interaction is not None:
             if self.interaction.k_c == 0:
-                self.eta = 0
+                eta = 0
             else:
                 # There is Coulomb, but we haven't (yet) worked out how to emulate
                 # across energies, so we can precompute H+ and H- stuff.
-                self.eta = self.interaction.eta(None)
+                eta = self.interaction.eta(None)
 
-        self.Hm = H_minus(self.s_0, self.interaction.ell, self.eta)
-        self.Hp = H_plus(self.s_0, self.interaction.ell, self.eta)
-        self.Hmp = H_minus_prime(self.s_0, self.interaction.ell, self.eta)
-        self.Hpp = H_plus_prime(self.s_0, self.interaction.ell, self.eta)
-        self.channels = np.zeros(1, dtype=jitr.channel_dtype)
-        self.channels["weight"] = np.ones(1)
-        self.channels["l"] = l
-        self.channels["a"] = a
-        self.channels["eta"] = self.eta
-        self.channels["Hp"] = np.array([self.Hp])
-        self.channels["Hm"] = np.array([self.Hm])
-        self.channels["Hpp"] = np.array([self.Hpp])
-        self.channels["Hmp"] = np.array([self.Hmp])
+        self.Hm = H_minus(self.s_0, self.interaction.ell, eta)
+        self.Hp = H_plus(self.s_0, self.interaction.ell, eta)
+        self.Hmp = H_minus_prime(self.s_0, self.interaction.ell, eta)
+        self.Hpp = H_plus_prime(self.s_0, self.interaction.ell, eta)
+
+        self.weight = np.ones(1)
+        self.l = np.array([interaction.ell])
+        self.a = s_0
+        self.eta = np.array([eta])
+        self.couplings = np.array([[0.]])
+
+        self.asym = jitr.reactions.Asymptotics(
+            np.array([self.Hp]),
+            np.array([self.Hm]),
+            np.array([self.Hpp]),
+            np.array([self.Hmp])
+        )
 
         # for Energized EIM interactions, E, mu k take up the first 3 spots
         # in the parameter vector, so we offset to account for that
@@ -59,12 +61,9 @@ class LagrangeRmatrix(SchroedingerEquation):
             self.potential = potential
             self.get_args = self.get_args_neutral
 
-        self.im = jitr.InteractionMatrix(1)
-        self.im.set_local_interaction(self.potential)
-
         # these are always parameter independent - we can precompute them
-        self.basis_boundary = self.solver.precompute_boundaries(a)
-        self.free_matrix = self.solver.free_matrix(a, l)
+        self.basis_boundary = self.solver.precompute_boundaries(self.a)
+        self.free_matrix = self.solver.free_matrix(self.a, self.l)
 
     def get_args_neutral(self, alpha):
         return (
@@ -88,12 +87,17 @@ class LagrangeRmatrix(SchroedingerEquation):
         return LagrangeRmatrix(interaction, self.s_0, self.solver)
 
     def get_channel_info(self, alpha):
-        self.im.local_args[0, 0] = self.get_args(alpha)
-        self.channels["E"] = self.interaction.E(alpha)
-        self.channels["mu"] = self.interaction.reduced_mass(alpha)
-        self.channels["k"] = self.interaction.momentum(alpha)
+        ch = jitr.reactions.Channels(
+            np.array([self.interaction.E(alpha)]),
+            np.array([self.interaction.momentum(alpha)]),
+            np.array([self.interaction.reduced_mass(alpha)]),
+            self.eta,
+            self.a,
+            self.l,
+            self.couplings,
+        )
 
-        return self.channels, self.im
+        return ch
 
     def phi(
         self,
@@ -103,21 +107,23 @@ class LagrangeRmatrix(SchroedingerEquation):
     ):
         assert s_mesh[-1] <= self.s_0
 
-        ch, im = self.get_channel_info(alpha)
+        ch = self.get_channel_info(alpha)
         R, S, x, uext_prime_boundary = self.solver.solve(
-            im,
             ch,
+            self.asym,
+            local_interaction=self.potential,
+            local_args=self.get_args(alpha),
             free_matrix=self.free_matrix,
             basis_boundary=self.basis_boundary,
             wavefunction=True,
         )
-        return jitr.Wavefunctions(
+        return jitr.reactions.wavefunction.Wavefunctions(
             self.solver,
             x,
             S,
             uext_prime_boundary,
-            self.channels["weight"],
-            jitr.make_channel_data(ch),
+            ch,
+            incoming_weights=self.weight,
         ).uint()[0](s_mesh)
 
     def smatrix(
@@ -125,12 +131,15 @@ class LagrangeRmatrix(SchroedingerEquation):
         alpha: np.array,
         **kwargs,
     ):
-        ch, im = self.get_channel_info(alpha)
-        R, S, uext_prime_boundary = self.solver.solve(
-            im,
+        ch = self.get_channel_info(alpha)
+        R, S, x, uext_prime_boundary = self.solver.solve(
             ch,
+            self.asym,
+            local_interaction=self.potential,
+            local_args=self.get_args(alpha),
             free_matrix=self.free_matrix,
             basis_boundary=self.basis_boundary,
+            wavefunction=False,
         )
         return S[0, 0]
 
@@ -139,11 +148,15 @@ class LagrangeRmatrix(SchroedingerEquation):
         alpha: np.array,
         **kwargs,
     ):
-        ch, im = self.get_channel_info(alpha)
-        R, S, uext_prime_boundary = self.solver.solve(
-            im,
+        ch = self.get_channel_info(alpha)
+        ch = self.get_channel_info(alpha)
+        R, S, x, uext_prime_boundary = self.solver.solve(
             ch,
+            self.asym,
+            local_interaction=self.potential,
+            local_args=self.get_args(alpha),
             free_matrix=self.free_matrix,
             basis_boundary=self.basis_boundary,
+            wavefunction=False,
         )
         return R[0, 0]
